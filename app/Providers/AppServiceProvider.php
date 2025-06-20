93ed6958362f17cb2ddd1f3347b74735879fd5e3
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-06-21 00:37:04 +0100

    boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Models\EventType;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -40,5 +41,7 @@ class AppServiceProvider extends ServiceProvider
             
             $view->with('eventTypes', $eventTypes);
         });
+
+        Schema::defaultStringLength(191);
     }
 }
