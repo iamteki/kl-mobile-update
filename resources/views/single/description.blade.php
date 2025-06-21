44578d672837428c251e6eba042ef389a0859bf5
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-06-21 13:57:12 +0100

    Animation V2

--- resources/views/single/description.blade.php
+++ resources/views/single/description.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-10">
-                    <div class="description-content text-center">
+                    <div class="description-content text-center"  data-animscroll="fade-up">
                         <p class="lead" style="font-size: 1.25rem; line-height: 1.8; margin-bottom: 2rem;">
                       {!! $event->description !!}
                         </p>
