12504202f41b8c2ab83d9f9df30dd79153862cc8
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-07-26 22:11:15 +0100

    Footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,17 +3,23 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <img src="{{ asset('frontend/assets/images/kl_logo_final_original.svg')}}" alt="KL Mobile Events" class="footer-logo">
-                    <p class="text-white-50">Your trusted partner for corporate events and DJ services in Kuala Lumpur
+                    <img src="{{ asset('frontend/assets/images/kl_logo_final_original.svg') }}" alt="KL Mobile Events"
+                        class="footer-logo">
+                    <p class="text-white-50">Your trusted partner for corporate event management, fabrication, sound,
+                        lighting, LED screens and full event production in Kuala Lumpur.
                     </p>
                 </div>
                 <div class="col-md-6 text-md-end">
                     <h5 class="mb-3 text-uppercase" style="color: var(--primary-purple);">Follow Us</h5>
                     <div class="social-links">
-                        <a href="https://www.facebook.com/share/1GFC7f5z4s/?mibextid=wwXIfr" class="social-icon" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
-                        <a href="https://www.instagram.com/" class="social-icon" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
-                        <a href="http://tiktok.com" class="social-icon" aria-label="TikTok"><i class="fab fa-tiktok"></i></a>
-                        <a href="http://linkedin.com" class="social-icon" aria-label="Linkedin"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="https://www.facebook.com/share/1GFC7f5z4s/?mibextid=wwXIfr" class="social-icon"
+                            aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://www.instagram.com/" class="social-icon" aria-label="Instagram"><i
+                                class="fab fa-instagram"></i></a>
+                        <a href="http://tiktok.com" class="social-icon" aria-label="TikTok"><i
+                                class="fab fa-tiktok"></i></a>
+                        <a href="http://linkedin.com" class="social-icon" aria-label="Linkedin"><i
+                                class="fab fa-linkedin-in"></i></a>
                     </div>
                 </div>
             </div>
