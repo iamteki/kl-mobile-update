fe4d6f8a01f8d29babd71f89d628054f0017aa43
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-07-31 10:33:38 +0100

    Bug Fix

--- resources/views/home/why-us.blade.php
+++ resources/views/home/why-us.blade.php
@@ -3,11 +3,11 @@
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-10 text-center">
-                    <div class="section-area">
+                    <div class="section-area" style="margin-bottom: 0px;">
                         <span data-animscroll="fade-up">- WHY CHOOSE US -</span>
                         <h2 class="fs-two text-white mb-4 upper" data-animscroll="fade-up">Importance of <span>Support</span></h2>
                     </div>
-                    <p data-animscroll="fade-up" class="lead" style="font-size: 1.25rem; line-height: 1.8;">
+                    <p data-animscroll="fade-up" class="lead" style="font-size: 1.25rem; line-height: 1;">
                         Celebrating a moment or milestone can be a once-in-a-lifetime opportunity, whether it's a
                         corporate or private event. These moments deserve to be captured and remembered. At KL Mobile
                         Events, we work towards making the moment unforgettable, because events are all about that moment . Once it's gone,
